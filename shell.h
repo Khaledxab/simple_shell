60c4de7f515f135b7bdd4df5fb5077ef7a8875b6
Ghassen-bgh <3591@holbertonschool.com>
2021-12-05 16:37:58 +0100

    HEADER FILE

--- shell.h
+++ shell.h
@@ -1,15 +1,47 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
+extern char **environ;
+extern char *program_invocation_name;
+
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
+
+/**
+ * struct built_in - Struct to handle the built-ins commands
+ * @command: input command to handle
+ * @f: function pointer to execute the command
+ *
+ * Description: has a command an function pointer to react to it
+ */
+typedef struct built_in
+{
+	char *command;
+	int (*f)(char **argv);
+} built_in_t;
 
 
-int main(int argc, char **argv);
-void shell_loop(void);
+int _cd(char **argv);
+int _help(char **args);
+int sh_exit(char **args);
+int _printenv(char **argv);
+int _setenv(char **argv);
+int _unsetenv(char **argv);
+char *_getenv(char *key);
+
+
+
+/* functions prototypes */
+void sign_handler(int sig);
+char *read_input();
+char **token_buff(char *buff, char *delimiter);
+
+int shell_execute(char **argv, built_in_t built_in_arr[]);
+int shell_launch(char **argv);
 
 
 #endif
